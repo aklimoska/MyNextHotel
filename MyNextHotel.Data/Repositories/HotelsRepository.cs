d6ce5614bd44d6cd8f00ccafaf32f49cbe17e218
aklimoska <angie.klimoskaa@gmail.com>
2018-07-16 14:49:54 +0200

    Added Repository Pattern and Entity Framework Models

---
+++ MyNextHotel.Data/Repositories/HotelsRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNextHotel.Data.Repositories
+{
+    class HotelsRepository : IHotelsRepository
+    {
+        private MyNextHotelDbContext _myNextHotelContext;
+
+        public HotelsRepository()
+        {
+            _myNextHotelContext = new MyNextHotelDbContext();
+        }
+        public void AddHotel(Hotel hotel, List<Room> rooms)
+        {
+            //if (hotel == null) throw new ArgumentNullException("hotel");
+            var savedHotel = _myNextHotelContext.Hotels.Add(hotel);
+            _myNextHotelContext.Rooms.AddRange(rooms);
+            _myNextHotelContext.SaveChanges();
+        }
+
+        public IQueryable<Hotel> GetAllHotels()
+        {
+            return _myNextHotelContext.Hotels;
+        }
+
+        public Hotel GetHotelByCityId(int id)
+        {
+            return _myNextHotelContext.Hotels.Where(x => x.CityID == id).FirstOrDefault();
+        }
+
+        public Hotel GetHotelById(int id)
+        {
+            return _myNextHotelContext.Hotels.Where(x => x.HotelID == id).FirstOrDefault();
+        }
+
+        public void RemoveHotel(Hotel hotel)
+        {
+            _myNextHotelContext.Hotels.Remove(hotel);
+            _myNextHotelContext.SaveChanges();
+        }
+    }
+}
---
+++ MyNextHotel.Data/Repositories/IHotelsRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyNextHotel.Data;
+
+namespace MyNextHotel.Data.Repositories
+{
+    interface IHotelsRepository
+    {
+        IQueryable<Hotel> GetAllHotels();
+        Hotel GetHotelById(int id);
+        Hotel GetHotelByCityId(int id);
+        void AddHotel(Hotel hotel, List<Room> rooms);
+        void RemoveHotel(Hotel hotel);
+    }
+}
